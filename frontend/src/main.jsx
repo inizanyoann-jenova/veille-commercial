7c17584d0436aa4e3581a26bab801676c5940628
inizanyoann-jenova <inizan.yoann@gmail.com>
2026-05-23 16:53:15 +0400

    feat(theme): load saved theme colors from localStorage on startup

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,6 +4,9 @@ import { BrowserRouter } from 'react-router-dom'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import App from './App'
 import './index.css'
+import { loadSavedTheme } from './utils/theme'
+
+loadSavedTheme()
 
 const queryClient = new QueryClient({
   defaultOptions: {
