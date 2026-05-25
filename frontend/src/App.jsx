35e4dcd3d64c75d4d1212cd9d1cee4984d4408a9
inizanyoann-jenova <inizan.yoann@gmail.com>
2026-05-25 14:20:29 +0400

    fix(App): Dashboard en accueil (index), Pipeline déplacé à /pipeline

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom'
 import Layout from './components/Layout'
 import Dashboard from './pages/Dashboard'
 import Analytics from './pages/Analytics'
+import Pipeline from './pages/Pipeline'
 import Direction from './pages/Direction'
 import Urgences from './pages/Urgences'
 import Parametres from './pages/Parametres'
@@ -12,6 +13,7 @@ export default function App() {
     <Routes>
       <Route path="/" element={<Layout />}>
         <Route index element={<Dashboard />} />
+        <Route path="pipeline" element={<Pipeline />} />
         <Route path="analytics" element={<Analytics />} />
         <Route path="direction" element={<Direction />} />
         <Route path="urgences" element={<Urgences />} />
